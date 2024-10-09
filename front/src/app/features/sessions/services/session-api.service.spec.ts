efb8046547a370f0f93130cd477e6aaacf3ee5a9
Yoan <naoy21@icloud.com>
2024-10-09 01:19:48 -0400

    Implement session services tests

--- front/src/app/features/sessions/services/session-api.service.spec.ts
+++ front/src/app/features/sessions/services/session-api.service.spec.ts
@@ -1,22 +1,104 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { expect } from '@jest/globals';
-
 import { SessionApiService } from './session-api.service';
+import { Session } from '../interfaces/session.interface';
 
-describe('SessionsService', () => {
+describe('SessionApiService', () => {
   let service: SessionApiService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
-      ]
+      imports: [HttpClientTestingModule],
+      providers: [SessionApiService]
     });
     service = TestBed.inject(SessionApiService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get all sessions', () => {
+    const mockSessions: Session[] = [
+      { id: 1, name: 'Session 1', description: 'Description 1', date: new Date(), teacher_id: 1, users: [] },
+      { id: 2, name: 'Session 2', description: 'Description 2', date: new Date(), teacher_id: 2, users: [] }
+    ];
+
+    service.all().subscribe(sessions => {
+      expect(sessions).toEqual(mockSessions);
+    });
+
+    const req = httpMock.expectOne('api/session');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockSessions);
+  });
+
+  it('should get session details', () => {
+    const mockSession: Session = { id: 1, name: 'Session 1', description: 'Description 1', date: new Date(), teacher_id: 1, users: [] };
+
+    service.detail('1').subscribe(session => {
+      expect(session).toEqual(mockSession);
+    });
+
+    const req = httpMock.expectOne('api/session/1');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockSession);
+  });
+
+  it('should delete a session', () => {
+    service.delete('1').subscribe();
+
+    const req = httpMock.expectOne('api/session/1');
+    expect(req.request.method).toBe('DELETE');
+    req.flush({});
+  });
+
+  it('should create a session', () => {
+    const newSession: Session = { name: 'New Session', description: 'New Description', date: new Date(), teacher_id: 1, users: [] };
+    const createdSession: Session = { id: 3, name: 'New Session', description: 'New Description', date: new Date(), teacher_id: 1, users: [] };
+
+    service.create(newSession).subscribe(session => {
+      expect(session).toEqual(createdSession);
+    });
+
+    const req = httpMock.expectOne('api/session');
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(newSession);
+    req.flush(createdSession);
+  });
+
+  it('should update a session', () => {
+    const updatedSession: Session = { id: 1, name: 'Updated Session', description: 'Updated Description', date: new Date(), teacher_id: 1, users: [] };
+
+    service.update('1', updatedSession).subscribe(session => {
+      expect(session).toEqual(updatedSession);
+    });
+
+    const req = httpMock.expectOne('api/session/1');
+    expect(req.request.method).toBe('PUT');
+    expect(req.request.body).toEqual(updatedSession);
+    req.flush(updatedSession);
+  });
+
+  it('should participate in a session', () => {
+    service.participate('1', 'user1').subscribe();
+
+    const req = httpMock.expectOne('api/session/1/participate/user1');
+    expect(req.request.method).toBe('POST');
+    req.flush({});
+  });
+
+  it('should unparticipate from a session', () => {
+    service.unParticipate('1', 'user1').subscribe();
+
+    const req = httpMock.expectOne('api/session/1/participate/user1');
+    expect(req.request.method).toBe('DELETE');
+    req.flush({});
   });
 });
--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -1,17 +1,69 @@
 import { TestBed } from '@angular/core/testing';
-import { expect } from '@jest/globals';
-
 import { SessionService } from './session.service';
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
 
 describe('SessionService', () => {
   let service: SessionService;
+  let mockUser: SessionInformation;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(SessionService);
+    mockUser = {
+      id: 1,
+      token: 'token',
+      type: 'user',
+      username: 'testuser',
+      firstName: 'Test',
+      lastName: 'User',
+      admin: false,
+    };
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('$isLogged', () => {
+    it('should return an Observable of isLogged', (done) => {
+      service.$isLogged().subscribe((isLogged) => {
+        expect(isLogged).toBeFalsy();
+        done();
+      });
+    });
+  });
+
+  describe('logIn', () => {
+    it('should set isLogged to true and update sessionInformation', () => {
+      service.logIn(mockUser);
+      expect(service.isLogged).toBeTruthy();
+      expect(service.sessionInformation).toEqual(mockUser);
+    });
+
+    it('should emit true on $isLogged Observable', (done) => {
+      service.$isLogged().subscribe((isLogged) => {
+        expect(isLogged).toBeTruthy();
+        done();
+      });
+      service.logIn(mockUser);
+    });
+  });
+
+  describe('logOut', () => {
+    it('should set isLogged to false and clear sessionInformation', () => {
+      service.logIn(mockUser);
+      service.logOut();
+      expect(service.isLogged).toBeFalsy();
+      expect(service.sessionInformation).toBeUndefined();
+    });
+
+    it('should emit false on $isLogged Observable', (done) => {
+      service.logIn(mockUser);
+      service.$isLogged().subscribe((isLogged) => {
+        expect(isLogged).toBeFalsy();
+        done();
+      });
+      service.logOut();
+    });
   });
 });
